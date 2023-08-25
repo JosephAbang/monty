8bf9e623697d186ccb9a34e6d3688a9260768ecd
JosephAbang <abangjosef@gmail.com>
2023-08-25 03:23:17 +0100

    Add a new line

--- op_func.c
+++ op_func.c
@@ -69,7 +69,7 @@ void cmd_div(stack_t **stack, unsigned int line_number)
 	second_top = top->next;
 	if (top->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		free_stack(stack);
 		fclose(stream);
                 exit(EXIT_FAILURE);
